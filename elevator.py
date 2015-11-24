4b3c7f50d7ac77f5fc3ad6e23b99fccb78cc07d9
Tomás Cariño Bazán <satirmo@gmail.com>
2015-11-24 14:41:17 -0500

    Update elevator.py

--- elevator.py
+++ elevator.py
@@ -1,14 +1,15 @@
 import random
-import simulation
 
 class Elevator:
 
-    id
-    status = { "ready":True, "traveling":False}
+    #id
+    status = {"ready":True, "traveling":False}
     currentFloor = 1
     destinations = []
-
-    #floorAccess
+    destinations.extend(range(1, 82))
+    floorAccess = 0
+    holding = 0
+    doorsOpen = False
 
     def __init__(self):
         self.id = None
@@ -18,65 +19,93 @@ class Elevator:
         return self.currentFloor
 
     def setFloor(self, floor):
-        self.currentFloor = floor
+        self.currentFloor = 1
+        holding = 0
+        doorsOpen = True
 
 
 
 
 class Retail_Elevator(Elevator):
     status = "ready"
-    #floorAccess
+    floorAccess = range(1, 6)
     currentFloor = 1
-    destinationFloor = 1
+
 
     def __init__(self):
         self.status = "ready"
         self.currentFloor = 1
-        self.destinationFloor = 1
 
 
 
 
-class Residential_Elevator(Elevator):
+class Residential_Elevator_Lower(Elevator):
 
-    id
     status = "ready"
     currentFloor = 1
-    destinationFloor = 1
-    #floorAccess
+    floorAccess = range(23, 40)
 
     def __init__(self):
-        self.id
         self.status = "ready"
         self.currentFloor = 1
-        self.destinationFloor = 1
+
+
+class Residential_Elevator_Luxury(Elevator):
+
+    status = "ready"
+    currentFloor = 1
+    floorAccess = range(63, 80)
+
+    def __init__(self):
+        self.status = "ready"
+        self.currentFloor = 1
 
 
 
-class Office_Elevator(Elevator):
+class Office_Elevator_Lower(Elevator):
 
-    id
     status ="ready"
     currentFloor = 1
-    destinationFloor = 1
-    #floorAccess
+    floorAccess = range(7, 20)
+
+class Office_Elevator_Luxury(Elevator):
+
+    status ="ready"
+    currentFloor = 1
+    floorAccess = range(43, 60)
 
 
 class Mechanical_Elevator(Elevator):
 
-    id
     status="ready"
     currentFloor = 1
-    destinationFloor = 1
-    #floorAccess
+    floorAccess = range(1, 81)
 
     def __init__(self):
-        self.id
         self.status = "ready"
         self.currentFloor = 1
-        self.destinationFloor = 1
 
 
+class Observatory_Elevator(Elevator):
+
+    status="ready"
+    currentFloor = 1
+    floorAccess = range(1, 81)
+
+    def __init__(self):
+        self.status = "ready"
+        self.currentFloor = 1
+
+
+class Key_Card_Elevator(Elevator):
+
+    status="ready"
+    currentFloor = 1
+    floorAccess = range(1, 81)
+
+    def __init__(self):
+        self.status = "ready"
+        self.currentFloor = 1
 
 
 
@@ -87,6 +116,3 @@ print "Status of retail elevator is", e1.status
 e1.setFloor(2)
 
 print e1.getFloor()
-
-
-
