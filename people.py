59f1b01e75bbe054ba0823016845d97514b1dd4c
Tomas Carino Bazan <satirmo@gmail.com>
2015-12-01 01:19:40 -0500

    Delete people.py

--- people.py
+++
@@ -1,9 +0,0 @@
-import elavator
-import random
-
-class People:
-    destinationFloor = random.randint(0,100)
-
-class Group(People):
-    currentFloor = random.randint(0,100)
-    numPeople = random.randint(1,12)
